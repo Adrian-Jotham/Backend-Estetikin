ce6e09298727d3e7a450f257d9035aed531e9503
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2023-06-07 14:50:53 +0700

    Editing, Adding Token Check to Other API
    isLoggedIn is middleware for token check, adding expired token checker.

--- routes/artikel.js
+++ routes/artikel.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const articleController = require("../controllers/artikel.js");
+const { isLoggedIn } = require('../controllers/auth');
 // const apiController = require('../controllers/app.js');
 const router = express.Router();
 
-router.get('/:type', articleController.getArticle);
+router.get('/:type',isLoggedIn,articleController.getArticle);
 
 
 module.exports = router;
--- routes/module.js
+++ routes/module.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const moduleController = require("../controllers/module.js");
 // const apiController = require('../controllers/app.js');
+const { isLoggedIn } = require('../controllers/auth');
 const router = express.Router();
 
-router.get('/module', moduleController.getStories);
+router.get('/module',isLoggedIn,moduleController.getStories);
 
 
 module.exports = router;
