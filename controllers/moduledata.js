cdf111d7cab3fb7a8ecb6ad35bca6e2b5f497451
Adrian <adrianferdinand19@gmail.com>
2023-06-10 09:15:16 +0700

    repairing  module

--- controllers/moduledata.js
+++ controllers/moduledata.js
@@ -40,13 +40,101 @@ exports.data =
             title : 'ini judul 2',
             description : 'ini deskripsi dari si judulnya',
             genre : 'hardcore',
-            url : 'https://keren123'
+            url : 'https://keren123',
+            content : 
+                [
+                    {
+                        id : 1,
+                        title : 'Buka aplikasi kamera pada device yang anda gunakan',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/EvanJotham.JPG'
+                    },
+                    {
+                        id:2,
+                        title : 'Tekan layar anda pada bagian gambar yang ingin diambil',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/IgelBobo.JPG'
+                    },
+                    {
+                        id:3,
+                        title : 'Setelah kamera sudah memberi preview gambar yang lebih jernih, tekan tombol shutter.',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/StragelMakan.JPG'
+                    },
+                    {
+                        id:4,
+                        title : 'Gambar dengan lensa fokus telah berhasil ditangkap.',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/StragelMakan.JPG'
+                    },
+                ],
         },
         {
             id : 3,
             title : 'ini judul 3',
             description : 'ini deskripsi dari si judulnya',
             genre : 'bedess',
-            url : 'https://keren123'
+            url : 'https://keren123',
+            content : 
+                [
+                    {
+                        id : 1,
+                        title : 'Buka aplikasi kamera pada device yang anda gunakan',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/EvanJotham.JPG'
+                    },
+                    {
+                        id:2,
+                        title : 'Tekan layar anda pada bagian gambar yang ingin diambil',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/IgelBobo.JPG'
+                    },
+                    {
+                        id:3,
+                        title : 'Setelah kamera sudah memberi preview gambar yang lebih jernih, tekan tombol shutter.',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/StragelMakan.JPG'
+                    },
+                    {
+                        id:4,
+                        title : 'Gambar dengan lensa fokus telah berhasil ditangkap.',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/StragelMakan.JPG'
+                    },
+                ],
+        },
+        {
+            id : 4,
+            title : 'ini judul 4',
+            description : 'ini deskripsi dari si judulnya',
+            genre : 'bedess',
+            url : 'https://keren123',
+            content : 
+                [
+                    {
+                        id : 1,
+                        title : 'Buka aplikasi kamera pada device yang anda gunakan',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/EvanJotham.JPG'
+                    },
+                    {
+                        id:2,
+                        title : 'Tekan layar anda pada bagian gambar yang ingin diambil',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/IgelBobo.JPG'
+                    },
+                    {
+                        id:3,
+                        title : 'Setelah kamera sudah memberi preview gambar yang lebih jernih, tekan tombol shutter.',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/StragelMakan.JPG'
+                    },
+                    {
+                        id:4,
+                        title : 'Gambar dengan lensa fokus telah berhasil ditangkap.',
+                        description : '-',
+                        url : 'https://storage.googleapis.com/assetmodule/StragelMakan.JPG'
+                    },
+                ],
         }
     ]
